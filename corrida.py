b9a15855d6b8ca39baabd24c3a3ca61b870e0a72
Bruno Passuello <bruno.fogazzi.passuello@gmail.com>
2024-06-10 12:53:02 -0300

    Update corrida.py

--- corrida.py
+++ corrida.py
@@ -1,28 +1,32 @@
-ncarros, nvoltas = [int(x) for x in input().split()]
-soma = 0
-primeiro = 0
-iprimeiro = 0
-segundo = 0
-isegundo = 0
-terceiro = 0
-iterceiro = 0
-i = 1
-for _ in range(ncarros):
-    tempos = [int(x) for x in input().split()]
-    soma = sum(tempos)
-    if soma > terceiro and soma > segundo and soma > primeiro:
-        iprimeiro = i
-        primeiro = soma
-    else:
-        if soma > terceiro and soma > segundo and soma < primeiro:
-            isegundo = i
-            segundo = soma
-        else:
-            if soma > terceiro and soma < segundo and soma < primeiro:
-                iterceiro = i
-                terceiro = soma
+qnt_carros, n_voltas = [int(x) for x in input().split()]
+
+lugares = [99999990,99999995,99999999]
+i1 = 0
+i2 = 0
+i3 = 0
+i = 0
+for _ in range(qnt_carros):
+    tempo = ([int(x) for x in input().split()])
+    tempo_somado = sum(tempo)
     i += 1
-    soma = 0
-print(iprimeiro)
-print(isegundo)
-print(iterceiro)
+    if tempo_somado < lugares[2] and tempo_somado > lugares[1]:
+        lugares[2] = tempo_somado
+        i3 = i        
+    if tempo_somado < lugares[1] and tempo_somado > lugares[0]:
+        lugares[2] = lugares[1]
+        lugares[1] = tempo_somado
+        i3 = i2
+        i2 = i        
+    if tempo_somado < lugares[0] :
+        lugares[2] = lugares[1]
+        lugares[1] = lugares[0]
+        lugares[0] = tempo_somado
+        i3 = i2
+        i2 = i1
+        i1 = i     
+print(i1)
+print(i2)
+print(i3)
+
+    
+            
