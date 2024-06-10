0e74c9649c2d50fc6884d6c7bcfc27392a48a513
Bruno Passuello <bruno.fogazzi.passuello@gmail.com>
2024-06-10 11:27:16 -0300

    Update abreviar.py

--- abreviar.py
+++ abreviar.py
@@ -1,13 +1,12 @@
-nome = str(input().split())
-
-
-
-
-
-print(nome[0])
-
-
-
-
-
-
+nome = input().split()
+novo_nome = ''
+i = 0
+nome_2 = nome[1:-1]
+for palavra in nome_2:
+    if palavra == 'dos' or palavra == 'da' or palavra == 'de' or palavra == 'das' or palavra == 'e':
+        novo_nome += ' ' + palavra 
+        break
+    
+    else:
+        novo_nome += ' ' + palavra[0] + '.'
+print(f"{nome[0]}{novo_nome} {nome[-1]}")
